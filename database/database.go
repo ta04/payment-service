349920fdd64913d2cbf1d9c2b43a157f71fc6761
SleepingNext <veroskevin8@gmail.com>
2020-03-08 13:05:57 +0700

    Add postgres database connection file

---
+++ database/database.go
@@ -0,0 +1,25 @@
+// product-service/database/database.go
+
+package database
+
+import (
+	"database/sql"
+	"fmt"
+)
+
+const (
+	host     = "database"
+	port     = 5432
+	user     = "sleepingnext"
+	password = "kevin99123"
+	dbname   = "products"
+)
+
+// OpenPostgresConnection is to connect to postgres database
+func OpenPostgresConnection() (*sql.DB, error) {
+	connStr := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := sql.Open("postgres", connStr)
+
+	return db, err
+}
