8796a15bae208b98660bf34d83caff393c9097e8
SleepingNext <veroskevin8@gmail.com>
2020-03-11 12:00:45 +0700

    Integrate with JWT

--- go.mod
+++ go.mod
@@ -3,12 +3,10 @@ module github.com/SleepingNext/payment-service
 go 1.13
 
 require (
+	github.com/SleepingNext/auth-service v0.0.0-20200310114554-2079cc787758
 	github.com/golang/protobuf v1.3.4
-	github.com/lib/pq v1.2.0
+	github.com/lib/pq v1.3.0
 	github.com/micro/go-micro v1.18.0
-	github.com/spf13/cobra v0.0.6 // indirect
 	golang.org/x/net v0.0.0-20200301022130-244492dfa37a
 	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e // indirect
-	golang.org/x/sys v0.0.0-20191220142924-d4481acd189f // indirect
-	gopkg.in/yaml.v2 v2.2.4 // indirect
 )
--- main.go
+++ main.go
@@ -3,8 +3,14 @@
 package main
 
 import (
+	"context"
+	"errors"
+	"github.com/micro/go-micro/client"
+	"github.com/micro/go-micro/metadata"
+	"github.com/micro/go-micro/server"
 	"log"
 
+	authPB "github.com/SleepingNext/auth-service/proto"
 	"github.com/SleepingNext/payment-service/database"
 	"github.com/SleepingNext/payment-service/handler"
 	paymentPB "github.com/SleepingNext/payment-service/proto"
@@ -46,5 +52,29 @@ func main() {
 	err = s.Run()
 	if err != nil {
 		log.Fatal(err)
+	}
+}
+
+
+func AuthWrapper(fn server.HandlerFunc) server.HandlerFunc {
+	return func(ctx context.Context, req server.Request, res interface{}) error {
+		meta, ok := metadata.FromContext(ctx)
+		if !ok {
+			return errors.New("no auth meta-data found in the request")
+		}
+
+		token := meta["Token"]
+		log.Println("authenticating with token: ", token)
+
+		// Validate the token
+		authClient := authPB.NewAuthServiceClient("go.micro.srv.auth", client.DefaultClient)
+		_, err := authClient.ValidateToken(context.Background(), &authPB.Token{
+			Token: token,
+		})
+		if err != nil {
+			return err
+		}
+		err = fn(ctx, req, res)
+		return err
 	}
 }
