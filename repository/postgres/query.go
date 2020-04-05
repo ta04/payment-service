900784da553dbb3922b7c1f3a9016dad7a8978b7
SleepingNext <veroskevin8@gmail.com>
2020-04-05 17:40:14 +0700

    Update query and repository files

--- repository/postgres/query.go
+++ repository/postgres/query.go
@@ -13,24 +13,25 @@ type Repository struct {
 	DB *sql.DB
 }
 
+// Index will index all active payments
 func (repo *Repository) Index() (payments []*paymentPB.Payment, err error) {
-	var id, orderId int32
+	var id, orderID int32
 	var paymentType, picture, status string
 
-	query := "SELECT * FROM payments"
+	query := "SELECT * FROM payments WHERE status = 'active'"
 	rows, err := repo.DB.Query(query)
 	if err != nil {
 		return nil, err
 	}
 
 	for rows.Next() {
-		err := rows.Scan(&id, &orderId, &paymentType, &picture, &status)
+		err := rows.Scan(&id, &orderID, &paymentType, &picture, &status)
 		if err != nil {
 			return nil, err
 		}
 		payment := &paymentPB.Payment{
 			Id:      id,
-			OrderId: orderId,
+			OrderId: orderID,
 			Type:    paymentType,
 			Picture: picture,
 			Status:  status,
@@ -41,24 +42,25 @@ func (repo *Repository) Index() (payments []*paymentPB.Payment, err error) {
 	return payments, err
 }
 
+// IndexByUserID will index all active payments by it's userID
 func (repo *Repository) IndexByUserID(user *paymentPB.User) (payments []*paymentPB.Payment, err error) {
-	var id, orderId int32
+	var id, orderID int32
 	var paymentType, picture, status string
 
-	query := fmt.Sprintf("SELECT * FROM payments WHERE user_id = %d", user.Id)
+	query := fmt.Sprintf("SELECT * FROM payments WHERE user_id = %d AND status = 'active'", user.Id)
 	rows, err := repo.DB.Query(query)
 	if err != nil {
 		return nil, err
 	}
 
 	for rows.Next() {
-		err := rows.Scan(&id, &orderId, &paymentType, &picture, &status)
+		err := rows.Scan(&id, &orderID, &paymentType, &picture, &status)
 		if err != nil {
 			return nil, err
 		}
 		payment := &paymentPB.Payment{
 			Id:      id,
-			OrderId: orderId,
+			OrderId: orderID,
 			Type:    paymentType,
 			Picture: picture,
 			Status:  status,
@@ -69,19 +71,20 @@ func (repo *Repository) IndexByUserID(user *paymentPB.User) (payments []*payment
 	return payments, err
 }
 
+// Show will show an active payment by it's id
 func (repo *Repository) Show(payment *paymentPB.Payment) (*paymentPB.Payment, error) {
-	var id, orderId int32
+	var id, orderID int32
 	var paymentType, picture, status string
 
-	query := fmt.Sprintf("SELECT * FROM payments WHERE id=%d", payment.Id)
-	err := repo.DB.QueryRow(query).Scan(&id, &orderId, &paymentType, &picture, &status)
+	query := fmt.Sprintf("SELECT * FROM payments WHERE id = %d AND status = 'active'", payment.Id)
+	err := repo.DB.QueryRow(query).Scan(&id, &orderID, &paymentType, &picture, &status)
 	if err != nil {
 		return nil, err
 	}
 
 	return &paymentPB.Payment{
 		Id:      id,
-		OrderId: orderId,
+		OrderId: orderID,
 		Type:    paymentType,
 		Picture: picture,
 		Status:  status,
--- repository/postgres/repository.go
+++ repository/postgres/repository.go
@@ -8,24 +8,27 @@ import (
 	paymentPB "github.com/SleepingNext/payment-service/proto"
 )
 
+// Store will store a new payment
 func (repo *Repository) Store(payment *paymentPB.Payment) (*paymentPB.Payment, error) {
 	query := fmt.Sprintf("INSERT INTO payments (order_id, type, picture, status)"+
-		" VALUES ('%d', '%s', '%s', '%s')", payment.OrderId, payment.Type, payment.Picture, payment.Status)
+		" VALUES ('%d', '%s', '%s', 'active')", payment.OrderId, payment.Type, payment.Picture)
 	_, err := repo.DB.Exec(query)
 
 	return payment, err
 }
 
+// Update will update an existing payment's data
 func (repo *Repository) Update(payment *paymentPB.Payment) (*paymentPB.Payment, error) {
-	query := fmt.Sprintf("UPDATE payments SET order_id = '%d', type = '%s', picture = %s, status = '%s'"+
-		" WHERE id = %d", payment.OrderId, payment.Type, payment.Picture, payment.Status, payment.Id)
+	query := fmt.Sprintf("UPDATE payments SET order_id = '%d', type = '%s', picture = %s, status = 'active'"+
+		" WHERE id = %d", payment.OrderId, payment.Type, payment.Picture, payment.Id)
 	_, err := repo.DB.Exec(query)
 
 	return payment, err
 }
 
+// Destroy will update an existing payment's status to inactive
 func (repo *Repository) Destroy(payment *paymentPB.Payment) (*paymentPB.Payment, error) {
-	query := fmt.Sprintf("DELETE FROM payments WHERE id=%d", payment.Id)
+	query := fmt.Sprintf("UPDATE payments SET status='inactive' WHERE id=%d", payment.Id)
 	_, err := repo.DB.Exec(query)
 
 	return payment, err
